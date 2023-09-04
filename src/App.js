8715b7d5cc603c62be2461adc45f44a64e51351a
ally haas <allyhaas@gmail.com>
2023-09-04 16:52:52 -0500

    added a grid with the layout that i want

--- src/App.js
+++ src/App.js
@@ -1,25 +1,29 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    return (
+        <section className="container">
+            <div className="blank-space1"></div>
+            <div className="title">READIT</div>
+            <div className="search">SEARCH BOOK USING GOOGLE BOOK API</div>
+            <img
+                className="img"
+                src="/pexels.jpg"
+                alt="Description of the image"
+            />
+            <div className="blank-space2"></div>
+            <div className="container2">
+                <div className="signup">SIGNUP</div>
+                <div className="login">LOGIN</div>
+                <div className="logout">LOGOUT</div>
+            </div>
+            <div className="blank-space3"></div>
+            <div className="container3">
+            <div className="want-to-read">want to read</div>
+            <div className="read">READ</div>
+            </div>
+        </section>
+    );
 }
 
 export default App;
